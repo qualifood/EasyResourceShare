7a81b77ea0219588b851ddd7e7bea98323e7d7c3
Sebasch <sebastian.schoberth@gmail.com>
2015-08-14 23:00:30 +0200

    Initial Project

---
+++ IamUsingIt/IamUsingIt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IamUsingIt.MvcApplication" Language="C#" %>
---
+++ IamUsingIt/IamUsingIt/Models/IdentityModels.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.Entity.Core.EntityClient;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IamUsingIt.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("Sequelizer", throwIfV1Schema: false)
+        {
+        }
+
+        public ApplicationDbContext(string connectionstring)
+            : base(connectionstring, throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            var result = new ApplicationDbContext();
+#if DEBUG
+            result = new ApplicationDbContext("LocalDebug");
+            Debug.WriteLine(result.Database.Connection.ConnectionString);
+#endif
+
+            return result;
+        }
+    }
+}
